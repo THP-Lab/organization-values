939ba6110dc31a6129341eeb6d72e4caa5997096
blaiseup <balazs.mozes@consensys.net>
2025-01-28 11:30:13 +0100

    chore: initial commit

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,137 @@
+import localFont from "next/font/local";
+
+const AtypDisplay = localFont({
+  src: [
+    {
+      path: "./AtypDisplay-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./AtypDisplay-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./AtypDisplay-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-LightItalic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./AtypDisplay-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-MediumItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./AtypDisplay-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-SemiboldItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./AtypDisplay-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./AtypDisplay-ThinItalic.woff2",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+  variable: "--font-atyp-display",
+  fallback: ["Arial", "sans-serif"],
+});
+
+const AtypText = localFont({
+  src: [
+    {
+      path: "./AtypText-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./AtypText-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./AtypText-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-LightItalic.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./AtypText-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-MediumItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./AtypText-Semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-SemiboldItalic.woff2",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./AtypText-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./AtypText-ThinItalic.woff2",
+      weight: "100",
+      style: "italic",
+    },
+  ],
+  variable: "--font-atyp-text",
+  fallback: ["Arial", "serif"],
+});
+
+export { AtypDisplay, AtypText };
