aba09bc0e6d4ed5d8373e0480fd3727445283d2d
blaiseup <balazs.mozes@consensys.net>
2025-02-11 15:46:23 +0100

    feat: add metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,31 @@ import { AtypDisplay, AtypText } from "@/fonts/fonts";
 import "../styles/global.scss";
 import Providers from "@/components/providers/Providers";
 
+/**
+ * @type {import('next').Metadata}
+ */
 export const metadata = {
   title: "Ethereum Values",
-  description: "Propose and vote on Ethereum values",
+  description:
+    "Join the community in an experiment to collectively align on the values at the core of Ethereum.",
+  openGraph: {
+    type: "website",
+    title: "Ethereum Values",
+    description:
+      "Join the community in an experiment to collectively align on the values at the core of Ethereum.",
+    siteName: "Ethereum Values",
+    images: [
+      {
+        url: "/images/og-image.png",
+      },
+    ],
+  },
+  robots: {
+    index: false,
+    follow: false,
+    noimageindex: true,
+    noarchive: true,
+  },
 };
 
 export default function RootLayout({ children }) {
