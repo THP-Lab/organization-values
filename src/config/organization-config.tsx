54a6cc0f81162da9c86d3447dfb7f1921e87b8a7
Sachathp <sacha.courbe1@gmail.com>
2025-04-23 23:15:03 +0200

    dynamic faq

--- src/config/organization-config.tsx
+++ src/config/organization-config.tsx
@@ -60,7 +60,40 @@ export const organizationConfig = {
     ]
   },
   
+  // FAQ items - chaque answer sera transformé en JSX dans le composant
+  faq: [
+    {
+      question: "What's this all about?",
+      answer: `This platform allows people to propose, discuss, and vote on values they feel are important to the organization.
 
+The objective is to see how certain mechanisms and forums can be used to encourage honest dialogue and a synthesis of diverse perspectives as a means to establish a community-created values system. While the outcome is unforeseen, we are hoping that the community will converge on values that are important to them.`,
+      links: [
+        {
+          text: "Intuition Systems",
+          url: "https://www.intuition.systems/"
+        }
+      ]
+    },
+    {
+      question: "How does it work?",
+      answer: `Anyone can propose a value, which includes a name and a description. When you propose a value, you must pay a small fee. This fee goes into a pool for that proposed value, and these fees are shared pro-rata between everyone that votes on the value.
+
+You can vote for or against a value that you propose. The amount you deposit to vote can be as little or as much as you want. Other users can subsequently vote 'for' or 'against' the value you proposed by depositing ETH. You don't need to propose a value to participate, as you can deposit ETH to vote 'for' or 'against' existing values, or simply participate in the discussion on the forum if there is a linked forum thread.
+
+The values that are proposed by the community will be stack-ranked according to the total ETH deposit for, less the total ETH deposited against, for each respective value.`
+    },
+    {
+      question: "What sort of values can I propose?",
+      answer: `You are free to propose any sort of values that you wish. These can be serious proposals to which you've given thought and consideration, or they can be frivolous. Be aware that frivolous proposals are less likely to see serious engagement and may end up dropping to the bottom of the rank (and therefore wasting your proposed fee and limiting any potential earnings).`
+    },
+    {
+      question: "Why is there a fee to participate?",
+      answer: `When you deposit ETH in a value, voting either "for" or "against", there is a small fee that is paid into the pool for that value. When you withdraw ETH, there is a small withdrawal fee. The fees paid into the pool are shared between participants that have some ETH in the pool at that time.
+
+This means that if you deposit some ETH into the pool for a specific value, you will get a percentage of all deposit and withdrawal fees that are paid by participants that deposit and withdraw after you deposit, and before you withdraw.`
+    },
+    
+  ],
   
   // External links and resources
   externalLinks: {
